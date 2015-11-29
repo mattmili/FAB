ef11e093259328d0bba060bdc597c3a5d4f278fc
AlexanderGladu <alexander.gladu@gmail.com>
2015-11-28 20:42:57 -0500

    Display
    Needed a test layout to use for the list view. Only displays content,
    and not source for now.

--- FAB/app/src/main/java/com/csci4100/fab/Result.java
+++ FAB/app/src/main/java/com/csci4100/fab/Result.java
@@ -5,12 +5,17 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Result extends AppCompatActivity {
 
@@ -64,8 +69,37 @@ public class Result extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String json) {
+            try {
+                populateView(json);
+            } catch (org.json.JSONException e) {
+                Log.d("JSON Error", e.toString());
+            }
             Log.d("Result", json);
         }
+    }
+
+    public void populateView(String jsonString) throws org.json.JSONException {
+        ParseDisplay display = new ParseDisplay(jsonString);
+
+        TextView title = (TextView) findViewById(R.id.book_title);
+        TextView author = (TextView) findViewById(R.id.name_of_author);
+        TextView reviewLabel = (TextView) findViewById(R.id.review_label);
+        ListView reviews = (ListView) findViewById(R.id.review_list);
+
+        title.setText(display.getTitle());
+        author.setText(display.getAuthor());
+        reviewLabel.setText("Displaying " + display.getReviews().size() + " of " + display.getNumReviews());
+
+        /**
+         * Temp array for only review values, add sources after
+         */
+        ArrayList<String> tempReview = new ArrayList<>();
+        for (int i = 0; i < display.getReviews().size(); i++) {
+            tempReview.add(display.getReviews().get(i).get(0));
+        }
+
+        ListAdapter listAdapter = new ArrayAdapter<>(this, R.layout.row_layout, tempReview);
+        reviews.setAdapter(listAdapter);
     }
 
 
